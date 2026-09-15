baec7bb29c02ef2b3b75b45f8768536ab1184590
van <v.tugusow@yandex.ru>
2026-09-15 11:57:43 +0300

    try to fix pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/SpaceX_Launches/',
+    base: '/SpaceX_Launches_3.2.3_Tugusov_Ivan/',
     plugins: [react()],
     test: {
         globals: true,
